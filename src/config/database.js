9c268db935b2b88bb07441b34fd5b2cb0ecf1363
davi gerson <dgas1@discente.ifpe.edu.br>
2024-12-30 17:59:04 -0300

    Primeiro commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'root',
+    database: 'IfBurger',
+    define: {
+        timespamps: true,
+        underscored: true,
+        underscoredALL: true,
+    },
+}
