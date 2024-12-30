1301e430a232d6bc062d1168c095ea752ed5526c
davi gerson <dgas1@discente.ifpe.edu.br>
2024-12-30 20:22:02 -0300

    #

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express"
+import { v4 } from "uuid"
+import User from "./app/models/User"
 
 const routes = new Router()
 
-routes.get("/", (request,response) =>{
-    return response.json ({message: "Olá cidadão"})
+routes.get("/", async (request,response) =>{
+    const user = await User.create({
+        id: v4(),
+        name:'Davi',
+        email:'gdavialves22@gmail.com',
+        password_hash:'onepiece'
+    })
+    return response.json (user)
 })
 
 export default routes
