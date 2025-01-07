6fa39bc354625b58b05f5f153a08cd940c264bb4
davi gerson <dgas1@discente.ifpe.edu.br>
2025-01-07 17:18:58 -0300

    adicionado função de update para categorias

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -4,39 +4,48 @@ import User from '../models/User'
 
 class CategoryController {
     async store(request, response) {
-        const schema = Yup.object().shape({
-            name: Yup.string().required(),
-        })
-
         try {
-            await schema.validateSync(request.body, { abortEarly: false })
-        } catch (err) {
-            return response.status(400).json({ error: err.errors })
-        }
+            const schema = Yup.object().shape({
+                name: Yup.string().required(),
+            })
 
-        const { admin: isAdmin } = await User.findByPk(request.userId)
-
-        if (!isAdmin) {
-            return response.status(401).json({ error: 'usuário não autorizado' })
-        }
-
-        const { name } = request.body
-
-        const categoryExists = await Category.findOne({
-            where: {
-                name,
+            try {
+                await schema.validateSync(request.body, { abortEarly: false })
+            } catch (err) {
+                return response.status(400).json({ error: err.errors })
             }
-        })
 
-        if (categoryExists) {
-            return response.status(400).json({ error: 'Categoria já existe' })
+            const { admin: isAdmin } = await User.findByPk(request.userId)
+
+            if (!isAdmin) {
+                return response
+                    .status(401)
+                    .json({ error: 'usuário não autorizado' })
+            }
+
+            const { name } = request.body
+
+            const { filename: path } = request.file
+
+            const categoryExists = await Category.findOne({
+                where: {
+                    name,
+                },
+            })
+
+            if (categoryExists) {
+                return response
+                    .status(400)
+                    .json({ error: 'Categoria já existe' })
+            }
+
+
+            const { id } = await Category.create({ name, path })
+
+            return response.json({ id, name })
+        } catch (err) {
+            console.log(err)
         }
-
-
-        const { id } = await Category.create({ name });
-
-
-        return response.json({ id, name })
     }
 
     async index(request, response) {
@@ -44,6 +53,57 @@ class CategoryController {
 
         return response.json(category)
     }
+
+
+    //update de categorias 
+
+    async update(request, response) {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string(),
+            })
+
+            try {
+                await schema.validateSync(request.body, { abortEarly: false })
+            } catch (err) {
+                return response
+                    .status(400)
+                    .json({ error: err.errors })
+            }
+
+            const { admin: isAdmin } = await User.findByPk(request.userId)
+
+            if (!isAdmin) {
+                return response
+                    .status(401)
+                    .json({ error: 'usuário não autorizado' })
+            }
+
+            const { name } = request.body
+
+            const { id } = request.params
+
+            const category = await Category.findByPk(id)
+
+            if (!category) {
+                return response
+                    .status(401)
+                    .json({ error: 'Categoria não existe.' })
+            }
+
+            let path
+            if (request.file) {
+                path = request.file.filename
+            }
+
+
+            await Category.update({ name, path}, { where: { id } } )
+
+        return response.status(200).json()
+    } catch(err) {
+        console.log(err)
+    }
+}
 
 }
 
